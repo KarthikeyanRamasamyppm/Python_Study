a5f14a33359d34aed727666f906448de96e6c88f
Karthikeyan 0277 <Karthikeyan.0277@mnw.local>
2022-08-18 23:32:08 +0530

    initial commit

---
+++ day2/day2_list.py
@@ -0,0 +1,23 @@
+txt="List are ordered and changeable ie mutable and allow duplicates"
+print(txt)
+#convert txt to list
+s=txt.split()
+print(s)
+
+#create a empty list
+
+fruits=[]
+print(len(fruits))
+
+# list with items
+
+fruits1=["apple","orange","banana","mango"]
+print(fruits1)
+
+#len of the fruits1
+print(len(fruits1)) 
+
+# add one more fruit to the existing fruit
+
+fruits1.append("kiwi")
+print(fruits1)
